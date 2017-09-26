a6bcbd722432ccf7c8e1b76d62b1d0f03a9f061b
Vishal Raj <vishalraj1802@gmail.com>
2017-09-26 09:44:14 +0530

    bug fixes

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ class App extends Component {
     const data=[]
     for(let x=0;x<30;x++){
       const random=Math.random()
-      const temp= data.length>0?data[data.length-1]:50
+      const temp= data.length>0?data[data.length-1].y:50
       const y= random >0.45? temp+Math.floor(random*20):temp-Math.floor(random*20)
       data.push({x,y})
     }
+    console.log(data)
     return data
   }
   render() {
